046cf0b80218ff6d657f712ee7654fb6e58ea748
Mazlum Altay <mazlumaltayy@outlook.com>
2021-03-08 18:00:27 +0300

    Add project files.

---
+++ Dictionary001/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary001
+{
+    class MyDictionary<T, K>
+    {
+        T[] keys;
+        K[] values;
+
+        public MyDictionary()
+        {
+            keys = new T[0];
+            values = new K[0];
+        }
+
+        public void Add(T key, K value)
+        {
+            T[] tempKey = keys;
+            K[] tempValue = values;
+            keys = new T[keys.Length + 1];
+            values = new K[values.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                values[i] = tempValue[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public T[] TItems
+        {
+            get { return keys; }
+        }
+        public K[] KItems
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ Dictionary001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dictionary001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> meyveler = new MyDictionary<int, string>();
+            meyveler.Add(4, "Elma");
+            for (int i = 0; i < meyveler.KItems.Length; i++)
+            {
+                Console.WriteLine(meyveler.KItems[i]);
+            }
+        }
+    }
+}
